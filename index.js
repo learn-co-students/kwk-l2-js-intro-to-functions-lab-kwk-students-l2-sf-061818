b694201016f3b3801ece01b92ac0eb2bf3ec9482
Areeta Wong <areetaw@gmail.com>
2018-06-21 18:48:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+
+function getUKTime() {
+  return new Date().getHours() + 5
+}
+
+function discountTicket(tixPrice) {
+  return tixPrice * .8;
+}
+
+function tweetReply(fanName) {
+  return "Hey " + fanName + ", thanks so much for the support! I really appreciate it!";
+}
