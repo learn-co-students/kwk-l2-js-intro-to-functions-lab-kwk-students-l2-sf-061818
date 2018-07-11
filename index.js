7d1f128daae4f018ae0a5c6ac72fa4d795712f09
Vivian Phung <vivianphung@outlook.com>
2018-07-11 22:06:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 // Write your code in this file!
 
+function getUKTime() {
+  return new Date().getHours() + 8;
+}
 
+getUKTime()
+
+function discountTicket(number) {
+  return number * 0.80
+  
+}
+function tweetReply(name) {
+  return "Hey " + name + ", thanks so much for the support! I really appreciate it!"
+}
